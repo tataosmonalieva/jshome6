3c1fd31c4097bfb8240fe82a37e76e1df5bee864
tataosmonalieva <emusya1010@mail.ru>
2023-11-02 13:18:30 +0600

    t

---
+++ script.js
@@ -0,0 +1,147 @@
+//Ожидаемый результат: [1, 2, 3] => [3, 2, 1] 
+//let num = [1,2,3]
+//console.log(num.reverse())
+
+//Ожидаемый результат: [0, 1, false, 2, undefined, '', 3, null] => [1, 2, 3]
+//let a = [0, 1, false, 2, undefined, '', 3, null]
+//console.log(a.filter(el => el !==0 && el !== false && el !== undefined && el !=='' && el !== null)) 
+
+//Ожидаемый результат: [1, 2, 3, 1, 2] => [1, 2, 3]
+//let a = [1, 2, 3, 1, 2]
+//console.log(a) -
+
+//Даны два массива: ['a', 'b', 'c'] и [1, 2, 3]. Объедините их вместе.
+//let a =  ['a', 'b', 'c']
+//let b = [1, 2, 3]
+//let c = [...a,...b]
+//console.log(c)
+
+//Дан массив [1, 2, 3]. Добавьте ему в конец элементы 4, 5, 6.
+ //let a = [1, 2, 3]
+ //let b = [ 4, 5, 6]
+ //let c = [...a,...b]
+//console.log(c)
+
+
+//Дан массив [1, 2, 3]. Добавьте ему в начало элементы 4, 5, 6.
+//let a =[1, 2, 3] 
+//let b = [4, 5, 6.]
+//let c = [...b,...a]
+//console.log(c)
+
+//Дан массив [1, 2, 3, 4, 5]. С помощью метода splice преобразуйте массив в [1, 4, 5]
+//let a = [1, 2, 3, 4, 5]
+//console.log(a.filter(el => el !==2 && el !==3))
+
+//Дан объект {js:'test', jq: 'hello', css: 'world'}. Получите массив его ключей
+ 
+   //let  a =  {
+    //js: 'test',
+    //jq: 'hello',
+    //css:'world'
+ //}   
+//console.log(Object.keys(a)) 
+
+//['Капуста', 'Репа', 'Редиска', 'Морковка']
+//let a = ['Капуста', 'Репа', 'Редиска', 'Морковка']
+//console.log(a.join(','))
+
+
+
+//const cars = [
+   // {
+        //make: "Ford",
+       // model: "Mustang",
+       // year: 1969,
+        //color: "red",
+        //price: 25000
+    //},
+    //{
+        //make: "Ford",
+        //model: "F-150",
+        //year: 2017,
+        //color: "blue",
+        //price: 30000
+    //},
+    //{
+        //make: "Tesla",
+        //model: "Model S",
+       // year: 2018,
+        //color: "black",
+        //price: 120000
+    //},
+    //{
+       // make: "Chevrolet",
+       // model: "Camaro",
+        //year: 1970,
+        //color: "orange",
+        //price: 60000
+    //},
+    //{
+       //make: "Dodge",
+       // model: "Challenger",
+       // year: 2019,
+        //color: "red",
+        //price: 35000
+    //},
+    //{
+       // make: "Chevrolet",
+       // model: "Corvette",
+        //year: 2019,
+       // color: "blue",
+        //price: 90000
+    //},
+    //{
+        //make: "Tesla",
+        //model: "Model 3",
+        //year: 2019,
+        //color: "white",
+       // price: 40000
+    //},
+
+    //{
+       // make: "Mercedes",
+        //model: "C-Class",
+        //year: 2019,
+        //color: "black",
+       // price: 60000
+
+   // }
+//]
+   
+   //console.log(cars.filter(el => el.price >= 40000 && el.price <= 60000))
+  // console.log(cars.filter(el => el.year <= 2018))
+  
+ 
+
+     
+   
+
+  
+   
+ 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+  
